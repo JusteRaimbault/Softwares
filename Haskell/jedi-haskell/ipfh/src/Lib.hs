b1b0900dc8dda2fd7f45ade43989e989d90ab308
Juste Raimbault <juste.raimbault@polytechnique.edu>
2019-09-02 15:38:16 +0200

    .

---
+++ Haskell/jedi-haskell/ipfh/src/Lib.hs
@@ -0,0 +1,38 @@
+{- |
+Module      : Lib
+Description : Example of a library file.
+Copyright   : (c) 2018, TODO:<Author name here>
+License     : ISC
+Maintainer  : TODO:<example@example.com>
+Stability   : experimental
+Portability : POSIX
+
+Example of library file which is also used for testing the test suites.
+You can write a longer description of this module here and add @some markup@.
+
+-}
+module Lib
+  (
+    -- * Exported functions
+    inc
+  ) where
+
+import Protolude
+
+-- | Increment one 'Num' value.
+--
+--  >>> let answer = 42 :: Int
+--  >>> let prev = answer - 1
+--  >>> inc prev
+--  42
+--  >>> succ . Prelude.last . Prelude.take prev . iterate inc $ 1
+--  42
+--
+--  Properties:
+--
+--  prop> succ x == inc x
+--  prop> inc (negate x) == negate (pred x)
+--
+inc :: Int -- ^ value to increment
+       -> Int -- ^ result
+inc x = x + 1
